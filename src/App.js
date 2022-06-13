39296ea53805d2da039e887e82fa2e9431b97070
anvar-muzaffarli <prof.anvar2013@gmail.com>
2022-06-13 15:57:34 +0400

    Navbar yaradildi

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+import Navbar from './components/Navbar';
+function App() {
+  return (
+    // React Fragment ve ya JSX Fragment
+    <div className='container'>
+    <Navbar />
+    </div>
+  );
+}
+
+export default App;
